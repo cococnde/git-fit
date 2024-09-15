a0302c1db9ff0fcd1010a700d587a61b4650e911
Yahvannel <yahvannellopez@gmail.com>
2024-09-14 21:13:15 -0500

    added background image to landing page updated styling code in landingpage.css, added photo to assets folder, reworked imports in landingpage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,14 +1,32 @@
-import React, { useState, useEffect } from 'react'; // Ensure useState and useEffect are imported
+import React from 'react'; // Ensure useState and useEffect are imported
 import '../styles/LandingPage.css';
-import SearchExercises from './searchexercise.jsx'; // Import the SearchExercises component
+import LoginForm from '../components/LoginForm'; // Import the SearchExercises component
+
+import image from '../../assets/hiking.png';
+
+
+const styles = {
+background: {
+  backgroundImage: `url{${image}}`,
+  backgroundColor: "#7fc7d9",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+},
+};
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
+    <div className="landing-page" style={styles.backgroundPosition}>
       <main>
         <section id="search">
-          <SearchExercises /> {/* Render the SearchExercises component */}
+          <LoginForm /> {/* Render the SearchExercises component */}
         </section>
+        {/* <img className='image'
+    
+          src={image}
+          alt="hiking"
+          style={{ width: "100%", height: "auto" }}
+          /> */}
       </main>
     </div>
   );
